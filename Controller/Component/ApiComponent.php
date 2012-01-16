2a4ed1f761a4159e83fc8e9f845386582e4c2beb
Christian Winther <cw@nodes.dk>
2012-01-16 12:55:38 +0100

    Spaces => tabs

--- Controller/Component/ApiComponent.php
+++ Controller/Component/ApiComponent.php
@@ -11,170 +11,170 @@
  */
 class ApiComponent extends Component {
 
-    /**
-    * Reference to the current controller
-    *
-    * @var Controller
-    */
-    protected $Controller;
+	/**
+	* Reference to the current controller
+	*
+	* @var Controller
+	*/
+	protected $Controller;
 
-    /**
-    * Reference to the current request
-    *
-    * @var CakeRequest
-    */
-    protected $request;
+	/**
+	* Reference to the current request
+	*
+	* @var CakeRequest
+	*/
+	protected $request;
 
-    /**
-    * Reference to the current response
-    *
-    * @var CakeResponse
-    */
-    protected $response;
+	/**
+	* Reference to the current response
+	*
+	* @var CakeResponse
+	*/
+	protected $response;
 
-    public function initialize(Controller $Controller) {
-        $this->controller   = $Controller;
-        $this->request      = $Controller->request;
-        $this->response     = $Controller->response;
+	public function initialize(Controller $Controller) {
+		$this->controller	= $Controller;
+		$this->request		= $Controller->request;
+		$this->response		= $Controller->response;
 
-        Configure::write('ResponseObject', $this->response);
+		Configure::write('ResponseObject', $this->response);
 
-        // Ensure we can detect API requests
-        $this->configureRequestDetectors();
+		// Ensure we can detect API requests
+		$this->configureRequestDetectors();
 
-        // Don't do additional work if its'n not an API request
-        if (!$this->request->is('api')) {
-            return;
-        }
+		// Don't do additional work if its'n not an API request
+		if (!$this->request->is('api')) {
+			return;
+		}
 
-        // Switch to the API view class
-        $this->controller->viewClass = 'Api.Api';
+		// Switch to the API view class
+		$this->controller->viewClass = 'Api.Api';
 
-        // Read out the API token
-        $this->configureApiToken();
+		// Read out the API token
+		$this->configureApiToken();
 
-        // Enforce API authentication
-        $this->configureApiAccess();
-    }
+		// Enforce API authentication
+		$this->configureApiAccess();
+	}
 
-    public function hasError() {
-        return get_class($this->controller) == 'CakeErrorController';
-    }
+	public function hasError() {
+		return get_class($this->controller) == 'CakeErrorController';
+	}
 
-    public function beforeRedirect($Controller, $url, $status = null, $exit = true) {
-        if ($this->request->is('api')) {
-            if (empty($status)) {
-                $status = 302;
-            }
+	public function beforeRedirect($Controller, $url, $status = null, $exit = true) {
+		if ($this->request->is('api')) {
+			if (empty($status)) {
+				$status = 302;
+			}
 
-            // Make sure URls always is absolute
-            $url = Router::url($url, true);
+			// Make sure URls always is absolute
+			$url = Router::url($url, true);
 
-            $Controller->view = 'redirect';
-            switch($status) {
-                case 404:
-                    $this->response->statusCode(404);
-                    $this->response->send();
-                    die;
-                case 301:
-                case 302:
-                    $this->response->statusCode($status);
-                    $this->response->header(array('location' => $url));
-                    break;
-                default:
-                    break;
-            }
+			$Controller->view = 'redirect';
+			switch($status) {
+				case 404:
+					$this->response->statusCode(404);
+					$this->response->send();
+					die;
+				case 301:
+				case 302:
+					$this->response->statusCode($status);
+					$this->response->header(array('location' => $url));
+					break;
+				default:
+					break;
+			}
 
-            // Render the redirect view
-            $Controller->set(compact('url', 'status'));
-            $Controller->render();
+			// Render the redirect view
+			$Controller->set(compact('url', 'status'));
+			$Controller->render();
 
-            // Send the result and stop the request
-            $this->response->send();
-            $this->_stop();
-        }
-    }
+			// Send the result and stop the request
+			$this->response->send();
+			$this->_stop();
+		}
+	}
 
-    /**
-    * Ensures that the current request is validated for Authentication
-    *
-    */
-    protected function configureApiAccess() {
-        // Do not enforce authentication if the request isn't API
-        if (!$this->request->is('api')) {
-            return;
-        }
+	/**
+	* Ensures that the current request is validated for Authentication
+	*
+	*/
+	protected function configureApiAccess() {
+		// Do not enforce authentication if the request isn't API
+		if (!$this->request->is('api')) {
+			return;
+		}
 
-        // Don't enforce API access check if the request is public
-        if ($this->hasError()) {
-            return;
-        }
+		// Don't enforce API access check if the request is public
+		if ($this->hasError()) {
+			return;
+		}
 
-        // If its a public action, do not enforce API security checks
-        if (in_array($this->controller->action, $this->controller->publicActions)) {
-            return;
-        }
+		// If its a public action, do not enforce API security checks
+		if (in_array($this->controller->action, $this->controller->publicActions)) {
+			return;
+		}
 
-        // Do not enforce authentication if the request is already authenticated
-        if ($this->controller->Auth->user()) {
-            return;
-        }
+		// Do not enforce authentication if the request is already authenticated
+		if ($this->controller->Auth->user()) {
+			return;
+		}
 
-        // Deny access if no AccessToken is provided
-        if (!Configure::read('Platform.AccessToken')) {
-            throw new ForbiddenException('Permission denied, missing access token');
-        }
+		// Deny access if no AccessToken is provided
+		if (!Configure::read('Platform.AccessToken')) {
+			throw new ForbiddenException('Permission denied, missing access token');
+		}
 
-        // Deny access if the AccessToken isn't valid
-        if (!$this->controller->Auth->login()) {
-            throw new ForbiddenException('Permission denied, invalid access token');
-        }
-    }
+		// Deny access if the AccessToken isn't valid
+		if (!$this->controller->Auth->login()) {
+			throw new ForbiddenException('Permission denied, invalid access token');
+		}
+	}
 
-    /**
-    * Tries to find the AccessToken from a long range of entry points
-    *
-    */
-    protected function configureApiToken() {
-        $token = null;
+	/**
+	* Tries to find the AccessToken from a long range of entry points
+	*
+	*/
+	protected function configureApiToken() {
+		$token = null;
 
-        // Check if the token is posted to the action
-        if (!empty($this->request->data['token'])) {
-            $token = $this->request->data['token'];
-        }
-        // Check if the token is passed as a query argument (?token=$token)
-        elseif (!empty($this->request->query['token'])) {
-            $token = $this->request->query['token'];
-        }
-        // Check if the token is passed as named argument (/token:$token)
-        elseif (!empty($this->request->params['named']['token'])) {
-            $token = $this->request->params['named']['token'];
-        }
-        // Check if the token is passed as a HTTP header
-        elseif (!empty($_SERVER['HTTP_PLATFORM_TOKEN'])) {
-            $token = $_SERVER['HTTP_PLATFORM_TOKEN'];
-        }
+		// Check if the token is posted to the action
+		if (!empty($this->request->data['token'])) {
+			$token = $this->request->data['token'];
+		}
+		// Check if the token is passed as a query argument (?token=$token)
+		elseif (!empty($this->request->query['token'])) {
+			$token = $this->request->query['token'];
+		}
+		// Check if the token is passed as named argument (/token:$token)
+		elseif (!empty($this->request->params['named']['token'])) {
+			$token = $this->request->params['named']['token'];
+		}
+		// Check if the token is passed as a HTTP header
+		elseif (!empty($_SERVER['HTTP_PLATFORM_TOKEN'])) {
+			$token = $_SERVER['HTTP_PLATFORM_TOKEN'];
+		}
 
-        // Write the found AccessToken (if any)
-        Configure::write('Platform.AccessToken', $token);
-    }
+		// Write the found AccessToken (if any)
+		Configure::write('Platform.AccessToken', $token);
+	}
 
-    protected function configureRequestDetectors() {
-        // Add detector for json
-        $this->request->addDetector('json', array('callback' => function(CakeRequest $request) {
-            // The sure solution is to check if the extension is "json"
-            if (isset($request->params['ext']) && $request->params['ext'] === 'json') {
-                return true;
-            }
+	protected function configureRequestDetectors() {
+		// Add detector for json
+		$this->request->addDetector('json', array('callback' => function(CakeRequest $request) {
+			// The sure solution is to check if the extension is "json"
+			if (isset($request->params['ext']) && $request->params['ext'] === 'json') {
+				return true;
+			}
 
-            // Or try to sniff out the accept header
-            return $request->accepts('json');
-        }));
+			// Or try to sniff out the accept header
+			return $request->accepts('json');
+		}));
 
-        // Generic API check
-        $this->request->addDetector('api', array('callback' => function(CakeRequest $request) {
-            // Currently only checks if a request is JSON - but allows us to easily add other request formats
-            return $request->is('json');
-        }));
-    }
+		// Generic API check
+		$this->request->addDetector('api', array('callback' => function(CakeRequest $request) {
+			// Currently only checks if a request is JSON - but allows us to easily add other request formats
+			return $request->is('json');
+		}));
+	}
 }
--- View/ApiView.php
+++ View/ApiView.php
@@ -1,62 +1,62 @@
 <?php
 class ApiView extends View {
-    protected $apiFormat;
+	protected $apiFormat;
 
-    public function __construct($controller) {
-        parent::__construct($controller);
+	public function __construct($controller) {
+		parent::__construct($controller);
 
-        if ($this->request->is('json')) {
-            $this->apiFormat = 'json';
-        } else {
-            throw new Exception('Unknown API request format');
-        }
-    }
+		if ($this->request->is('json')) {
+			$this->apiFormat = 'json';
+		} else {
+			throw new Exception('Unknown API request format');
+		}
+	}
 
-    protected function _getViewFileName($name = null) {
-        // Handle Exceptions genericly for now
-        if ($this->viewPath === 'Errors') {
-            $this->layoutPath = 'json';
+	protected function _getViewFileName($name = null) {
+		// Handle Exceptions genericly for now
+		if ($this->viewPath === 'Errors') {
+			$this->layoutPath = 'json';
 
-            $old_plugin = $this->plugin;
-            $this->plugin = 'Api';
+			$old_plugin = $this->plugin;
+			$this->plugin = 'Api';
 
-            $file = parent::_getViewFileName('/' . $this->apiFormat . DS . 'exception');
+			$file = parent::_getViewFileName('/' . $this->apiFormat . DS . 'exception');
 
-            $this->plugin = $old_plugin;
-            unset($old_plugin);
+			$this->plugin = $old_plugin;
+			unset($old_plugin);
 
-            return $file;
-        }
+			return $file;
+		}
 
-        // try to find it with default
-        try {
-            return parent::_getViewFileName($name);
-        } catch (MissingViewException $e) { }
+		// try to find it with default
+		try {
+			return parent::_getViewFileName($name);
+		} catch (MissingViewException $e) { }
 
-        // Try default api views
-        try {
-            $this->viewPath = str_replace($this->apiFormat, 'api', $this->viewPath);
-            return parent::_getViewFileName($name);
-        } catch (MissingViewException $e) { }
+		// Try default api views
+		try {
+			$this->viewPath = str_replace($this->apiFormat, 'api', $this->viewPath);
+			return parent::_getViewFileName($name);
+		} catch (MissingViewException $e) { }
 
-        // Try default api action view
-        $old_plugin = $this->plugin;
-        $this->plugin = 'Api';
-        try {
-            $file = parent::_getViewFileName('/' . $this->apiFormat . '/' . $this->view);
-        } catch (MissingViewException $e) { }
+		// Try default api action view
+		$old_plugin = $this->plugin;
+		$this->plugin = 'Api';
+		try {
+			$file = parent::_getViewFileName('/' . $this->apiFormat . '/' . $this->view);
+		} catch (MissingViewException $e) { }
 
-        if (!empty($file)) {
-            // Reset plugin
-            $this->plugin = $old_plugin;
-            unset($old_plugin);
-            return $file;
-        }
+		if (!empty($file)) {
+			// Reset plugin
+			$this->plugin = $old_plugin;
+			unset($old_plugin);
+			return $file;
+		}
 
-        // Finally try default api view
-        $file = parent::_getViewFileName('/' . $this->apiFormat . '/default');
-        $this->plugin = $old_plugin;
-        unset($old_plugin);
-        return $file;
-    }
+		// Finally try default api view
+		$file = parent::_getViewFileName('/' . $this->apiFormat . '/default');
+		$this->plugin = $old_plugin;
+		unset($old_plugin);
+		return $file;
+	}
 }
