8250688efd31020b105d2e43619b45c528b97f08
Christian Winther <cw@nodes.dk>
2012-02-10 17:25:42 +0100

    Make compatible with Crud

--- Lib/Crud/Event/Api.php
+++ Lib/Crud/Event/Api.php
@@ -3,37 +3,36 @@ namespace Crud\Event;
 
 class Api extends Base {
 
-	public function init(\Controller $controller, $action = null) {
-		parent::init($controller, $action);
-		switch($this->action) {
+	public function init(\CakeEvent $event) {
+		switch($event->subject->action) {
 			case 'index':
 			case 'admin_index':
-				if (!$controller->request->is('get')) {
+				if (!$event->subject->request->is('get')) {
 					throw new \MethodNotAllowedException();
 				}
 				break;
 			case 'add':
 			case 'admin_add':
-				if (!$controller->request->is('post')) {
+				if (!$event->subject->request->is('post')) {
 					throw new \MethodNotAllowedException();
 				}
 				break;
 			case 'edit':
 			case 'admin_edit':
-				if (!$controller->request->is('put')) {
+				if (!$event->subject->request->is('put')) {
 					throw new \MethodNotAllowedException();
 				}
 				break;
 			case 'delete':
 			case 'admin_delete':
-				if (!$controller->request->is('delete')) {
+				if (!$event->subject->request->is('delete')) {
 					throw new \MethodNotAllowedException();
 				}
 				break;
 		}
 	}
 
-	public function recordNotFound($controller, $action) {
+	public function recordNotFound(\CakeEvent $event) {
 		throw new \FileNotFoundException();
 	}
 }
