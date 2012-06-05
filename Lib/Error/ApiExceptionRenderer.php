1b2f18eacabc0c52414b7967875dc372e0710ed5
AD7six <andydawson76@gmail.com>
2012-06-05 11:37:44 +0200

    don't hijack error messages
    Throwing any exception which has a code results in trying to render "error<code>.ctp"
    which, if it doens't exist will then be rendered as a missing view exception.
    
    This change will silently handle such cases without hijacking the error message

--- Lib/Error/ApiExceptionRenderer.php
+++ Lib/Error/ApiExceptionRenderer.php
@@ -2,13 +2,42 @@
 App::uses('ExceptionRenderer', 'Error');
 
 class ApiExceptionRenderer extends ExceptionRenderer {
-	/**
-	 * A safer way to render error messages, replaces all helpers, with basics
-	 * and doesn't call component methods.
-	 *
-	 * @param string $template The template to render
-	 * @return void
-	 */
+
+/**
+ * Generate the response using the controller object.
+ *
+ * If there is no specific template for the error raised (normally there will
+ * not be) swallow the missing view exception and just use the standard
+ * error format. This prevents throwing a RandomException and seeing instead
+ * a MissingView exception
+ *
+ * @param string $template The template to render.
+ * @return void
+ */
+	protected function _outputMessage($template) {
+		try {
+			$this->controller->render($template);
+			$this->controller->afterFilter();
+			$this->controller->response->send();
+		} catch (MissingViewException $e) {
+			$this->_outputMessageSafe('error500');
+		} catch (Exception $e) {
+			$this->controller->set(array(
+				'error' => $e,
+				'name' => $e->getMessage(),
+				'code' => $e->getCode(),
+			));
+			$this->_outputMessageSafe('error500');
+		}
+	}
+
+/**
+ * A safer way to render error messages, replaces all helpers, with basics
+ * and doesn't call component methods.
+ *
+ * @param string $template The template to render
+ * @return void
+ */
 	protected function _outputMessageSafe($template) {
 		$this->controller->layoutPath = '';
 		$this->controller->subDir = '';
